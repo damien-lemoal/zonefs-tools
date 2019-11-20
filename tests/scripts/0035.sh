21fa9e756f6c860c0ee7c95b88008bc6a7635ddc
Ting Yao <ting.yao1@wdc.com>
2019-11-20 10:58:00 +0900

    tests: Add test 0035
    Add test 0035 to check for the number of blocks with aggr_cnv format
    using stat command.
    
    Signed-off-by: Ting Yao <ting.yao1@wdc.com>

---
+++ tests/scripts/0035.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copyright (C) 2019 Western Digital Corporation or its affiliates.
+#
+
+. scripts/test_lib
+
+if [ $# == 0 ]; then
+        echo "Number of blocks using stat (aggr_cnv)"
+        exit 0
+fi
+
+echo "Check for number of blocks of the file system"
+
+zonefs_mkfs "-o aggr_cnv $1"
+zonefs_mount "$1"
+
+nr_blocks=$(block_number "$zonefs_mntdir")
+sz_blocks=$(block_size "$zonefs_mntdir")
+nr_expected_blocks=$(( (nr_zones - 1 ) * zone_bytes / sz_blocks ))
+
+if [ "$nr_blocks" != "$nr_expected_blocks" ]; then
+        echo " --> Invalid number of total number of blocks:"
+        echo " --> Expected $nr_expected_blocks, got $nr_blocks"
+        exit 1
+fi
+
+zonefs_umount
+
+exit 0
+
