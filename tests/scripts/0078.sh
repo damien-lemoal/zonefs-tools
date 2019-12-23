5a4f15646b8164f1e25234999c520970a28694c1
Damien Le Moal <damien.lemoal@wdc.com>
2019-12-23 12:40:05 +0900

    tests: Add mmap tests
    Add new test cases 0078, 0079 and 0089 to test mmap read and write.
    Especially check that mmap writing is prevented for sequential zone
    files.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>

---
+++ tests/scripts/0078.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copyright (C) 2019 Western Digital Corporation or its affiliates.
+#
+
+. scripts/test_lib
+
+if [ $# == 0 ]; then
+	echo "Conventional file mmap read/write"
+        exit 0
+fi
+
+if [ "$nr_cnv_files" == 0 ]; then
+	exit_skip
+fi
+
+echo "Check conventional file mmap write"
+
+zonefs_mkfs "$1"
+zonefs_mount "$1"
+
+maxsize=$(file_max_size "$zonefs_mntdir"/cnv/0)
+fio --name=cnv_mmapwr --filename="$zonefs_mntdir"/cnv/0 \
+    --rw=randwrite --ioengine=mmap --size="$maxsize" \
+    --create_on_open=0 --allow_file_create=0 --file_append=0 --unlink=0 \
+    --bs=4096 --verify=md5 --do_verify=1 --overwrite=1 \
+    --continue_on_error=none || \
+    exit_failed " --> FAILED"
+
+zonefs_umount
+
+echo "Check conventional file mmap read"
+
+zonefs_mount "$1"
+
+fio --name=cnv_mmaprd --filename="$zonefs_mntdir"/cnv/0 \
+    --rw=randread --ioengine=mmap -size="$maxsize" \
+    --create_on_open=0 --allow_file_create=0 --file_append=0 --unlink=0 \
+    --bs=4096 --verify=md5 --do_verify=1 --continue_on_error=none || \
+    exit_failed " --> FAILED"
+
+zonefs_umount
+
+exit 0
---
+++ tests/scripts/0089.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copyright (C) 2019 Western Digital Corporation or its affiliates.
+#
+
+. scripts/test_lib
+
+if [ $# == 0 ]; then
+	echo "Sequential file mmap read/write"
+        exit 0
+fi
+
+echo "Check sequential file mmap write (should fail)"
+
+zonefs_mkfs "$1"
+zonefs_mount "$1"
+
+# Fill the file for mmap
+maxsize=$(file_max_size "$zonefs_mntdir"/seq/0)
+truncate --no-create --size=$maxsize "$zonefs_mntdir"/seq/0 || \
+        exit_failed " --> FAILED"
+
+sz=$(file_size "$zonefs_mntdir"/seq/0)
+[ "$sz" != "$maxsize" ] && \
+        exit_failed " --> Invalid file size $sz B, expected $maxsize B"
+
+# mmpa(MAP_WRITE) should fail
+fio --name=cnv_mmapwr --filename="$zonefs_mntdir"/seq/0 \
+    --create_on_open=0 --allow_file_create=0 --file_append=0 --unlink=0 \
+    --rw=randwrite --ioengine=mmap --size="$sz" \
+    --bs=4096 --continue_on_error=none && \
+    exit_failed " --> FAILED"
+
+# Reset the file
+truncate --no-create --size=0 "$zonefs_mntdir"/seq/0 || \
+        exit_failed " --> FAILED"
+
+# Fill file
+fio --name=seqwrite --filename="$zonefs_mntdir"/seq/0 \
+    --create_on_open=0 --allow_file_create=0 --file_append=1 --unlink=0 \
+    --rw=write --ioengine=libaio --iodepth=64 \
+    --bs=4096 --size="$maxsize" --verify=md5 --do_verify=1 \
+    --continue_on_error=none --direct=1 || \
+	exit_failed "fio write FAILED"
+
+sz=$(file_size "$zonefs_mntdir"/seq/0)
+[ "$sz" != "$zone_bytes" ] && \
+	exit_failed " --> Invalid file size $sz B, expected $zone_bytes B"
+
+zonefs_umount
+
+echo "Check sequential file mmap read"
+
+zonefs_mount "$1"
+
+fio --name=seq_rndrd --filename="$zonefs_mntdir"/seq/0 \
+    --rw=randread --ioengine=mmap \
+    --create_on_open=0 --allow_file_create=0 --unlink=0 \
+    --bs=4096 --size="$maxsize" --verify=md5 --do_verify=1 \
+    --continue_on_error=none || \
+	exit_failed "fio mmap rand read FAILED"
+
+zonefs_umount
+
+exit 0
