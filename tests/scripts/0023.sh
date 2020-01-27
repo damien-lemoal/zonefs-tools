002025d95901d078d43ded144a7f7d0905661ac0
Damien Le Moal <damien.lemoal@wdc.com>
2020-01-27 15:48:48 +0900

    tests: New test 0023
    Add the new test script 0023.sh to test mount options.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>

---
+++ tests/scripts/0023.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0
+#
+# Copyright (C) 2019 Western Digital Corporation or its affiliates.
+#
+
+. scripts/test_lib
+
+if [ $# == 0 ]; then
+	echo "mount (options)"
+        exit 0
+fi
+
+zonefs_mkfs "$1"
+
+# Test good mount options
+OPTS=("-o errors=remount-ro"
+      "-o errors=continue"
+      "-o errors=panic")
+for ((i = 0; i < ${#OPTS[@]}; i++)); do
+	zonefs_mount "${OPTS[$i]} $1"
+	zonefs_umount "$1"
+done
+
+# Test invalid mount options
+OPTS=("-o errors=unknown"
+      "-o bad_option")
+for ((i = 0; i < ${#OPTS[@]}; i++)); do
+	zonefs_mount_err "${OPTS[$i]} $1"
+done
+
+exit 0
