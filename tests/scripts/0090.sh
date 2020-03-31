fa694f00e751f8c02957de590b7e64d43236e6ee
Johannes Thumshirn <johannes.thumshirn@wdc.com>
2020-03-31 20:51:53 +0900

    tests: test 4k sequential write to a sequential zone
    Test 4k sequential write to a sequential zone and verify the result. This
    is a basic test for zonefs' zone-append code path.
    
    Signed-off-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>

---
+++ tests/scripts/0090.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2020 Western Digital Corporation or its affiliates.
+#
+
+. scripts/test_lib
+
+if [ $# == 0 ]; then
+	echo "4k sequential file synchronous write"
+        exit 0
+fi
+
+set -x
+
+require_program fio
+
+echo "Check 4k sequential file sync write"
+
+zonefs_mkfs "$1"
+zonefs_mount "$1"
+
+sz=$(file_max_size "$zonefs_mntdir"/seq/0)
+truncate --no-create --size=0 "$zonefs_mntdir"/seq/0 || \
+        exit_failed " --> FAILED"
+
+fio --name=seqwrite --filename="$zonefs_mntdir"/seq/0 \
+    --create_on_open=0 --allow_file_create=0 --file_append=1 --unlink=0 \
+    --rw=write --ioengine=psync --iodepth=64 \
+    --bs=4096 --size="$sz" --verify=md5 --do_verify=1 \
+    --continue_on_error=none --direct=1 || \
+	exit_failed "fio write FAILED"
+
+zonefs_umount
+
+set +x
+
+exit 0
