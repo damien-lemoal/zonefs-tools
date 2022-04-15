b23826ac794be5755037c41c21d6f883b1388f95
Damien Le Moal <damien.lemoal@opensource.wdc.com>
2022-04-15 18:12:08 +0900

    tests: 0114: Check write-open seq file count with explicit-open
    Check that zonefs nr_wro_seq_files sysfs attribute value matches the
    expected value with seq files being open read or write when the
    explicit-open mount option is used. This new test case also checks that
    trying to open more that max_wro_seq_files fails.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>

---
+++ tests/scripts/0114.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2022 Western Digital Corporation or its affiliates.
+#
+
+. scripts/test_lib
+
+if [ $# == 0 ]; then
+	echo "Sysfs seq files write-open (explicit-open)"
+        exit 0
+fi
+
+require_sysfs
+
+zonefs_mkfs "$1"
+zonefs_mount "-o explicit-open $1"
+
+i=0
+maxopen=$(get_max_open_zones "$1")
+if [ ${maxopen} -eq 0 ]; then
+	maxopen=4
+fi
+
+echo "Check read open"
+
+# Check that any number of file can be read-open
+for((i=1; i<=$(( maxopen + 1 )); i++)); do
+	echo "Opening ${i} files for reading"
+
+	tools/zopen --nrfiles="$i" --fflag=read --pause "${zonefs_mntdir}/seq" &
+	zopid=$!
+	sleep 1
+
+	nract=$(sysfs_nr_active_seq_files "$1")
+	nrwro=$(sysfs_nr_wro_seq_files "$1")
+
+	kill ${zopid}
+	wait ${zopid}
+
+	[[ ${nract} -eq 0 ]] || \
+		exit_failed "nr_active_seq_files is ${nract} (should be 0)"
+	[[ ${nrwro} -eq 0 ]] || \
+		exit_failed "nr_wro_seq_files is ${nrwro} (should be 0)"
+done
+
+echo "Check write open"
+
+# Check that any number of file can be write-open
+for((i=1; i<=${maxopen}; i++)); do
+	echo "Opening ${i} files for writing"
+
+	tools/zopen --nrfiles="$i" --fflag=write --pause "${zonefs_mntdir}/seq" &
+	zopid=$!
+	sleep 1
+
+	nract=$(sysfs_nr_active_seq_files "$1")
+	[[ ${nract} -eq ${i} ]] || \
+		exit_failed "nr_active_seq_files is ${nract} (should be ${i})"
+
+	nrwro=$(sysfs_nr_wro_seq_files "$1")
+	[[ ${nrwro} -eq ${i} ]] || \
+		exit_failed "nr_wro_seq_files is ${nrwro} (should be ${i})"
+
+	# Close the files
+	kill ${zopid}
+	wait ${zopid}
+
+	# Now all counters should be back to 0 since the files where not written
+	nract=$(sysfs_nr_active_seq_files "$1")
+	[[ ${nract} -eq 0 ]] || \
+		exit_failed "nr_active_seq_files is ${nract} after close (should be 0)"
+
+	nrwro=$(sysfs_nr_wro_seq_files "$1")
+	[[ ${nrwro} -eq 0 ]] || \
+		exit_failed "nr_wro_seq_files is ${nrwro} after close (should be 0)"
+done
+
+# Opening one more file than max-open should fail
+echo "Opening $(( maxopen + 1 )) files for writing (failure expected)"
+tools/zopen --nrfiles="$(( maxopen + 1 ))" \
+	--fflag=write "${zonefs_mntdir}/seq" && \
+	exit_failed "Write-opening $(( maxopen + 1 )) > ${maxopen} files succedded (should fail)"
+
+sleep 1
+
+nract=$(sysfs_nr_active_seq_files "$1")
+[[ ${nract} -eq 0 ]] || \
+	exit_failed "nr_active_seq_files is ${nract} after close (should be 0)"
+
+nrwro=$(sysfs_nr_wro_seq_files "$1")
+[[ ${nrwro} -eq 0 ]] || \
+	exit_failed "nr_wro_seq_files is ${nrwro} after close (should be 0)"
+
+zonefs_umount
+
+exit 0
