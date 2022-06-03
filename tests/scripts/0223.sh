987999134341253a9f1d329d8999150ae96d2e46
Damien Le Moal <damien.lemoal@opensource.wdc.com>
2022-06-03 21:09:08 +0900

    tests: 0324: test conventional file read beyond eof
    Read past the size of a conventional zone file.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>

---
+++ tests/scripts/0223.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2022 Western Digital Corporation or its affiliates.
+#
+
+. scripts/test_lib
+
+if [ $# == 0 ]; then
+	echo "Conventional file read beyond eof"
+        exit 0
+fi
+
+echo "Check conventional file read beyond eof"
+
+zonefs_mkfs "$1"
+zonefs_mount "$1"
+
+bs=$(block_size "$zonefs_mntdir"/seq/0)
+
+sz=$(file_size "$zonefs_mntdir"/cnv/0)
+ofst=$(( ($sz - 512) / 512 ))
+dd if="$zonefs_mntdir"/cnv/0 of=/dev/null \
+    bs="$(( bs * 2 ))" count=1 seek="$ofst" || \
+    exit_failed " --> FAILED"
+
+zonefs_umount
+
+exit 0
