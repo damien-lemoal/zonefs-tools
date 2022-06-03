f7c27bfc9323c14d8f6708e5053be58cd6c0d914
Damien Le Moal <damien.lemoal@opensource.wdc.com>
2022-06-03 18:15:40 +0900

    tests: 0216: test conventional file sequential read
    Same as 0210 but with reads.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>

---
+++ tests/scripts/0216.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2022 Western Digital Corporation or its affiliates.
+#
+
+. scripts/test_lib
+
+if [ $# == 0 ]; then
+	echo "Conventional file seq read"
+        exit 0
+fi
+
+require_cnv_files
+require_program fio
+
+echo "Check conventional file seq read (sync)"
+
+zonefs_mkfs "$1"
+zonefs_mount "$1"
+
+fio --name=cnv_seqrd --filename="$zonefs_mntdir"/cnv/0 \
+    --rw=read --ioengine=psync \
+    --bs=131072 --continue_on_error=none || \
+    exit_failed " --> FAILED"
+
+zonefs_umount
+
+echo "Check conventional file seq read (async)"
+
+zonefs_mkfs "$1"
+zonefs_mount "$1"
+
+fio --name=cnv_seqrd --filename="$zonefs_mntdir"/cnv/0 \
+    --rw=read --ioengine=libaio --iodepth=64 \
+    --bs=131072 --continue_on_error=none || \
+    exit_failed " --> FAILED"
+
+zonefs_umount
+
+exit 0
