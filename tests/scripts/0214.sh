b27ae9e6087238136cd15be1def7f88edfe07664
Damien Le Moal <damien.lemoal@opensource.wdc.com>
2022-06-03 18:02:55 +0900

    tests: 0214: do not reformat between buffered and direct IO tests
    To exercise page cache invalidation.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>

--- tests/scripts/0214.sh
+++ tests/scripts/0214.sh
@@ -28,14 +28,7 @@ dd if=/dev/zero of="$zonefs_mntdir"/cnv/0 \
 
 sync || exit_failed " --> FAILED"
 
-zonefs_umount
-
 echo "Check conventional file seq write (large_IOs, direct)"
-
-zonefs_mkfs "$1"
-zonefs_mount "$1"
-
-bs=$(file_size "$zonefs_mntdir"/cnv/0)
 
 dd if=/dev/zero of="$zonefs_mntdir"/cnv/0 \
     bs="$bs" count=1 conv=notrunc oflag=direct || \
