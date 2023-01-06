18bdde7dc4a7029151362c78ec79d6215dc897fa
Damien Le Moal <damien.lemoal@opensource.wdc.com>
2023-01-06 16:21:26 +0900

    tests: Add offline zones error handling tests
    Add test cases 0620 to 0627 to test mount time and IO execution time
    handling of offline zones. These tests rely on the new zone_offline
    config attribute of the null_blk driver.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>

---
+++ tests/scripts/0621.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2023 Western Digital Corporation or its affiliates.
+#
+
+. scripts/test_lib
+
+if [ $# == 0 ]; then
+	echo "Sequential file offline zone (errors=remount-ro, io-err)"
+        exit 0
+fi
+
+echo "Check sequential file on an offline zone at io-error time"
+
+require_nullb_offline
+
+nulldev=$(create_zoned_nullb)
+dev="/dev/nullb${nulldev}"
+
+zonefs_mkfs "${dev}"
+zonefs_mount "${dev}"
+
+# Writing should be OK
+echo "Write seq file 0"
+write_file "${zonefs_mntdir}/seq/0" 4096
+
+# Set the first seq file zone offline
+set_nullb_first_seq_zone_offline "${nulldev}"
+
+# Writing should now fail
+echo "Try writing seq file 0"
+write_file_err "${zonefs_mntdir}/seq/0" 4096 0
+
+# Truncate should also fail
+echo "Try file truncate to 0 (zone reset)"
+truncate_file "${zonefs_mntdir}/seq/0" 0 && \
+	exit_failed " --> truncate SUCCESS (should FAIL)"
+check_file_size "${zonefs_mntdir}/seq/0" 0
+
+# File permission should have changed and the entire file system
+# should now be read-only
+echo "Check permissions"
+check_file_perm "${zonefs_mntdir}/seq/0" "000"
+check_fs_is_readonly
+
+# Evicting the inode should not change anything
+echo "Check file size and permissions after eviction"
+drop_inode_cache
+check_file_size "${zonefs_mntdir}/seq/0" 0
+check_file_perm "${zonefs_mntdir}/seq/0" "000"
+
+# We should not be able to write another file
+echo "Try writing seq file 1"
+write_file_err "${zonefs_mntdir}/seq/1" 4096 0
+
+zonefs_umount
+
+destroy_nullb ${nulldev}
+
+exit 0
---
+++ tests/scripts/0624.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# Copyright (C) 2023 Western Digital Corporation or its affiliates.
+#
+
+. scripts/test_lib
+
+if [ $# == 0 ]; then
+	echo "Sequential file offline zone (errors=zone-offline, mount)"
+        exit 0
+fi
+
+echo "Check sequential file on an offline zone at mount time"
+
+require_nullb_offline
+
+nulldev=$(create_zoned_nullb)
+dev="/dev/nullb${nulldev}"
+
+zonefs_mkfs "${dev}"
+zonefs_mount "${dev}"
+
+# Writing should be OK
+echo "Write seq file 0"
+write_file "${zonefs_mntdir}/seq/0" 4096
+
+# Unmount, set the first seq file zone offline and remount
+echo "Remounting FS with seq file 0 zone as offline"
+zonefs_umount "${dev}"
+set_nullb_first_seq_zone_offline "${nulldev}"
+zonefs_mount "-o errors=zone-offline ${dev}"
+
+# The file permission must have changed and the file size should be 0.
+# The fs should be writable
+echo "Check file size and permissions"
+check_file_size "${zonefs_mntdir}/seq/0" 0
+check_file_perm "${zonefs_mntdir}/seq/0" "000"
+check_fs_is_writable
+
+# Writing to the file should fail
+echo "Try writing seq file 0"
+write_file_err "${zonefs_mntdir}/seq/0" 4096 0
+
+# We should be able to write another file
+echo "Write seq file 1"
+write_file "${zonefs_mntdir}/seq/1" 4096
+
+zonefs_umount
+
+destroy_nullb ${nulldev}
+
+exit 0
