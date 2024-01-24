85c5642d4d6da821ce2684b213ac927321e1adf2
Shin'ichiro Kawasaki <shinichiro.kawasaki@wdc.com>
2024-01-24 09:45:14 +0900

    tests: 0002: Fix missing library source command and mkfs.f2fs failures
    The test case does not source scripts/test_lib then exit_failed() bash
    function defined in scripts/test_lib is not working. The test case was
    not able to report test failure correctly, and the following issues were
    hidden. Source the script library to not hide failures.
    
    On top of that, this test case fails at the mkfs.f2fs -m command when
    
     1) the test target device has a filesystem,
     2) the test target device does not have conventional zones, or
     3) the test target device size is larger than 16TB (f2fs design limit).
    
    To avoid the failures, add the -f option to mkfs.f2fs, check
    conventional zones existence and specify filesystem size up to 16TB.
    
    Signed-off-by: Shin'ichiro Kawasaki <shinichiro.kawasaki@wdc.com>
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>

--- tests/scripts/0002.sh
+++ tests/scripts/0002.sh
@@ -5,15 +5,22 @@
 # Copyright (C) 2019 Western Digital Corporation or its affiliates.
 #
 
+. scripts/test_lib
+
 if [ $# == 0 ]; then
 	echo "mkzonefs (force format)"
 	exit 0
 fi
 
 require_program mkfs.f2fs
+require_cnv_files
 
-# Format with f2fs
-mkfs.f2fs -m "$1" || exit_failed " --> f2fs format FAILED"
+# Format with f2fs. Cap filesystem size up to 16TB per f2fs design.
+sectors=$((nr_zones * zone_sectors))
+sectors_16TB=$((16 * 1024 * 1024 * 1024 * 1024 / 512))
+((sectors > sectors_16TB)) && sectors=$((sectors_16TB))
+f2fs_sectors=$((sectors / ($(get_logical_block_size "$1") / 512)))
+mkfs.f2fs -f -m "$1" $((f2fs_sectors)) || exit_failed " --> f2fs format FAILED"
 
 # This should fail
 echo "Check mkzonefs with used super block and not forced"
